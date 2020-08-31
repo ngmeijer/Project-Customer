91dd44ef50291f4702db98f06ce808b5d4f5060f
ngmeijer <meijernils9@gmail.com>
2020-08-31 15:32:45 +0200

    Created basic project layout.

---
+++ ProjectCustomer/Assets/Scripts/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private Animator canvasAnimator = null;
+    [SerializeField] private float sceneTransitionTime = 1f;
+
+    public IEnumerator StartGame()
+    {
+        canvasAnimator.SetTrigger("ProceedToLevel");
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        yield break;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
